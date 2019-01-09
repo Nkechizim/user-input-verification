0ddaae7d646a42982caa27ffcfdb922e5f229565
Deborah Chizim <deborah@Deborahs-MacBook-Pro.local>
2019-01-10 00:32:18 +0100

    Second Commit

--- app.js
+++ app.js
@@ -6,14 +6,13 @@ const joi = require('joi');
 const app = express();
 
 
-app.use('/public', express.static(path.join(__dirname, 'static')));
+//app.use('/public', express.static(path.join(__dirname, 'static')));
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
-app.get('/', (req,res)=>{
-    res.sendFile(path.join(__dirname, 'static', 'index.html'));
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname + '/index.html');
 });
-
 
 app.post('/', (req, res)=>{
     const schema = joi.object().keys({
