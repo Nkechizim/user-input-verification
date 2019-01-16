ed25dc868b96d523e9213a5b3e8f0d7c7e7f5354
Nkechizim <nkechizim.nc@gmail.com>
2019-01-16 19:08:24 +0100

    User Registration and Login - Authentication

--- app.js
+++ app.js
@@ -1,32 +1,75 @@
-const express = require('express');
-const path = require('path'); 
+const mongoose = require('mongoose');
+var user = require('./auth-model');
+const express = require('express'); 
 const bodyParser = require('body-parser');
-const joi = require('joi');
+const bcrypt = require('bcrypt-nodejs');
+
+var url = 'mongodb://localhost/user-authentication';
+
+mongoose.connect(url, function (err, db) {
+  if (err) {
+
+      console.log('Unable to connect to the mongoDB server. Error:', err);
+  }
+
+  else {
+      console.log('Connection established to', url);
+  }
+});
 
 const app = express();
 const server = require('http').createServer(app);
 
 server.listen(process.env.PORT || 3000); 
 
-//app.use('/public', express.static(path.join(__dirname, 'static')));
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
-app.get('/', (req, res)=>{
-    res.sendFile(__dirname + '/index.html');
+app.get('/register', (req, res)=>{
+  res.status(200);
+  res.sendFile(__dirname + '/index.html');
 });
 
-app.post('/', (req, res)=>{
-    const schema = joi.object().keys({
-        email : joi.string().trim().email().required(),
-        password : joi.string().min(6).max(15).required()
-    });
-    joi.validate(req.body, schema, (err, result)=>{
-        if(err){
-            console.log(err);
-            res.send('Your password must be between 6 and 15 characters!');
+app.get('/login', (req, res)=>{
+  res.status(200);
+  res.sendFile(__dirname + '/login.html');
+});
+
+app.post('/register', (req, res)=>{
+  new user({
+      Firstname: req.body.Firstname,
+      Lastname: req.body.Lastname,
+      PhoneNumber: req.body.PhoneNumber,
+      Email: req.body.Email,
+      Password: req.body.Password,
+  }).save(function(err, doc){
+      if(err){
+           res.status(400).send('Uh-Oh! Something Went Wrong');
+           console.log(err);
+      }else{
+          res.send('You have been registered successfully!');
+          console.log('You have been registered successfully!');
+      }
+  });
+});
+
+app.post('/login', (req, res)=>{
+  user.findOne({ PhoneNumber: req.body.PhoneNumber})
+      .exec(function (err, loginuser) {
+        if (err) {
+          res.status(400).send('Uh-Oh! Something Went Wrong');
+        } else if (!loginuser) {
+          res.status(401).send('User not found!');
         }
-        console.log(result);
-        res.send('Successful');
-    });
+        bcrypt.compare(req.body.Password, loginuser.Password, function (err, result) {
+          if (result === true) {
+            res.status(200).send('Login Successful!');
+            
+            //res.send(loginuser.toJSON());
+
+          } else {
+            res.status(400).send('Password is incorrect!');
+          }
+        })
+      });
 });
---
+++ auth-model.js
@@ -0,0 +1,90 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require ('mongoose-unique-validator');
+const bcrypt = require('bcrypt-nodejs');
+const hashSync = bcrypt.hashSync;
+const compareSync = bcrypt.compareSync;
+
+const AuthSchema = new mongoose.Schema({
+    Firstname:{
+      type: String,
+      trim: true,
+      required: true ['Firstname must be Provided!'],
+    },
+    Lastname:{
+      type: String,
+      trim: true,
+      required: false,
+    },
+    PhoneNumber:{
+      type: Number,
+      trim: true,
+      required: true ['Phone Number must be provided!'],
+      unique: true,
+      validate: {
+        validator(PhoneNumber){
+          //return PhoneNumber.length >= 10 && PhoneNumber.length <= 15;
+        },
+        message: '{VALUE} IS NOT A VALID PHONE NUMBER!',
+      },
+    },
+    Email:{
+      type: String,
+      unique: true,
+      dropDups: true,
+      trim: true,
+      required: true,
+      validate: {
+        validator(Email){
+          const emailRegex = /^[-a-z0-9%S_+]+(\.[-a-z0-9%S_+]+)*@(?:[a-z0-9]{1,63}\.){1,125}[a-z]{2,63}$/;
+          return emailRegex.test(Email);
+        },
+        message: '{VALUE} IS NOT A VALID EMAIL!',
+      },
+    },
+    Password:{
+      type: String,
+      trim: true,
+      required: true,
+      validate: {
+        validator(Password){
+          return Password.length >= 6 && Password.length <= 15;
+        },
+        message: 'PASSWORD MUST BE BETWEEN 6 & 15 CHARACTERS!',
+      },
+    },
+  });
+  
+  AuthSchema.plugin(uniqueValidator, {
+    message: '{VALUE} already exists!'
+  });
+  
+  AuthSchema.pre('save', function(next){
+    if(this.isModified('Password')){
+      this.Password = this._hashPassword(this.Password);
+      return next();
+    }
+    return next();
+  });
+  
+  
+  module.exports = {
+    _hashPassword(Password){
+        return hashSync(Password);
+      },
+
+         authenticateUser(Password){
+          return compareSync(Password, this.Password);
+        },
+    
+          toJSON(){
+            return{
+              _id: this._id,
+              Firstname: this.Firstname,
+              Lastname: this.Lastname,
+              PhoneNumber: this.PhoneNumber,
+              Email: this.Email,
+            };
+          },
+  }
+  
+module.exports = mongoose.model('Users', AuthSchema);
